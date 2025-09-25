42fd50864d478b2130c8e28d6b5e10a029beb1f4
BramVA01 <bramva02@gmail.com>
2025-09-25 11:05:04 +0200

    Add main function

--- Squared_number.py
+++ Squared_number.py
@@ -1,4 +1,3 @@
-
 original_number = 2
 
 def square(number):
@@ -9,5 +8,33 @@ print(square(original_number))
 
 
 def main():
-    pass
+    print(square(original_number))
+    print(give_solutions(limit_to_z))
+
+
+limit_to_z = 10
+"""def give_solutions(limit):
+    sol =[]
+    for i in range(0,limit):
+        equation = Eq(x**2 + y**2 - i**2)
+        sol = [sol,solve(equation)]
+    return sol"""
+
+def give_solutions(limit):
+    sol_x=[]
+    sol_y=[]
+    sol_i=[]
+    for i in range(0,limit):
+        for x in range(0,limit):
+            for y in range(0,limit):
+                if x**2 + y**2 == i**2:
+                    sol_x = [sol_x, x]
+                    sol_y = [sol_y, y]
+                    sol_i = [sol_i, i]
+    return sol_x,sol_y
+
+
+if __name__ == "__main__":
+    main()
+
 
